6cce798e94f746c3620e927026e3bd1146bf5f0b
Janek <makarony7@wp.pl>
2020-06-16 13:52:00 +0200

    many changes in menu and other

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,14 @@ function animateMenu() {
     var menu = $('.menuBg');
     var nav = $('.mobileNav');
 
-    if (menu.hasClass('showMenu')) { // Hide menu if it's open
+    if (menu.hasClass('showMenu')) {
         menu.removeClass('showMenu').addClass('hideMenu');
         nav.removeClass('fadeIn');
-    } else if (menu.hasClass('hideMenu')) { // Show menu and remove hideMenu
+    } else if (menu.hasClass('hideMenu')) {
         menu.removeClass('hideMenu').addClass('showMenu');
         nav.addClass('fadeIn')
     } else {
-        menu.addClass('showMenu'); // Initial show menu
+        menu.addClass('showMenu');
         nav.addClass('fadeIn');
     }
 };
@@ -25,49 +25,51 @@ $(document).ready(function () {
     });
 });
 
-// RWD NAVIGATION
 $(document).ready(function () {
-    var $navbar = $(".nav");
-
+    var $navbar = $(".nav-fixed");
     AdjustHeader();
     $(window).scroll(function () {
         AdjustHeader();
     });
-
     function AdjustHeader() {
         if ($(window).scrollTop() > 1) {
-            if (!$navbar.hasClass("padd")) {
-                $navbar.addClass("padd");
+            if (!$navbar.hasClass("nav-fixed-scroll")) {
+                $navbar.addClass("nav-fixed-scroll");
             }
         } else {
-            $navbar.removeClass("padd");
+            $navbar.removeClass("nav-fixed-scroll");
+        }
+    }
+});
+
+$(document).ready(function () {
+    var $navbar = $(".back-to-top");
+    AdjustHeader();
+    $(window).scroll(function () {
+        AdjustHeader();
+    });
+    function AdjustHeader() {
+        if ($(window).scrollTop() > 200) {
+            if (!$navbar.hasClass("back-to-top-show")) {
+                $navbar.addClass("back-to-top-show");
+            }
+        } else {
+            $navbar.removeClass("back-to-top-show");
         }
     }
 });
 
 // Active link switching
-var scrollLink = $('.scroll-p ');
+var scrollLink = $('.scroll-p');
 $(window).scroll(function () {
     var scrollbarLocation = $(this).scrollTop();
-
     scrollLink.each(function () {
-
         var sectionOffset = $(this.hash).offset().top - 73;
-
         if (sectionOffset <= scrollbarLocation) {
             $(this).parent().addClass('active');
             $(this).parent().siblings().removeClass('active');
         }
     });
-});
-
-// Show or hide the sticky footer button
-$(window).on('scroll', function (event) {
-    if ($(this).scrollTop() > 600) {
-        $('.back-to-top').fadeIn(200)
-    } else {
-        $('.back-to-top').fadeOut(200)
-    }
 });
 
 $(function() {
@@ -84,40 +86,3 @@ $(function() {
         }
     });
 });
-
-$( ".cl-1" ).click(function() {
-    $( ".cl-1" ).toggleClass("open");
-    $( "#details1" ).toggleClass("open");
-});
-$( ".cl-2" ).click(function() {
-    $( ".cl-2" ).toggleClass("open");
-    $( "#details2" ).toggleClass("open");
-});
-$( ".cl-3" ).click(function() {
-    $( ".cl-3" ).toggleClass("open");
-    $( "#details3" ).toggleClass("open");
-});
-$( ".cl-4" ).click(function() {
-    $( ".cl-4" ).toggleClass("open");
-    $( "#details4" ).toggleClass("open");
-});
-$( ".cl-5" ).click(function() {
-    $( ".cl-5" ).toggleClass("open");
-    $( "#details5" ).toggleClass("open");
-});
-$( ".cl-6" ).click(function() {
-    $( ".cl-6" ).toggleClass("open");
-    $( "#details6" ).toggleClass("open");
-});
-
-var c1 = document.querySelector('.cl-1');
-var c2 = document.querySelector('.cl-2');
-var c3 = document.querySelector('.cl-3');
-var c4 = document.querySelector('.cl-4');
-var c5 = document.querySelector('.cl-5');
-var c6 = document.querySelector('.cl-6');
-c1.onclick = function() {
-    
-}
-
-console.log(c1);
